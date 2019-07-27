500c19fde9f7022a62a7f06d32ddf6280bcd0e95
suyashak <suyash@appmozart.com>
2019-07-27 15:27:09 -0700

    tweaks and npm scope

--- aspen-icons.js
+++ aspen-icons.js
@@ -1,5 +1,5 @@
-import '/node_modules/@polymer/iron-iconset-svg/iron-iconset-svg.js';
-const $_documentContainer = document.createElement('template');
+import '@polymer/iron-iconset-svg/iron-iconset-svg.js';
+export const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = `<iron-iconset-svg name="aspen" size="24">
 <svg><defs>
